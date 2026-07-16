2ea48d32fda26067e8a6632bbac01c5e378864c2
nandoks <fernandooksman@gmail.com>
2026-07-16 02:55:40 +0200

    kickstart project

---
+++ src/main/java/com/br/nandoks/plant_oks/PlantOksApplication.java
@@ -0,0 +1,13 @@
+package com.br.nandoks.plant_oks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantOksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantOksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/nandoks/plant_oks/PlantOksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.nandoks.plant_oks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantOksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
